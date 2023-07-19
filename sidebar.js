47aab0a68e421244023acc0ee75e8d534676ffad
Alif8454 <malif201106@gmail.com>
2023-07-19 20:17:16 +0700

    first commit

---
+++ sidebar.js
@@ -0,0 +1,4 @@
+document.getElementById('menu_toggle').addEventListener('click', function() {
+    document.getElementById('check').checked = true; /* Mengaktifkan checkbox saat tombol ditekan */
+  });
+  
